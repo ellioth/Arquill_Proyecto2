d96ff607ad7827c0604bae54778946ea2a7a1602
mcquiddy <cristian95mc@gmail.com>
2019-10-18 17:15:49 -0600

    Add files via upload

--- app/SERIAL_PORT.ino
+++ app/SERIAL_PORT.ino
@@ -24,22 +24,32 @@ void loop() {
     digitalWrite(11, LOW);
     digitalWrite(10, LOW);
     digitalWrite(9, LOW);
+    //set hour
     if(serialData == '3'){
       digitalWrite(13, HIGH);
       digitalWrite(12, LOW);
+      Serial.write("3",1);
     }
+    //set alarm
     else if(serialData == '2') {
       digitalWrite(12, HIGH);
       digitalWrite(13, LOW);
+      Serial.write("2",1);
     }
+    //select 7 segment
     else if(serialData == '1') {
       digitalWrite(11, HIGH);
+      Serial.write("1",1);
     }
+    //up clock
     else if(serialData == '0') {
       digitalWrite(10, HIGH);
+      Serial.write("0",1);
     }
+    //set clock
     else if(serialData == '9') {
       digitalWrite(9, HIGH);
+      Serial.write("9",1);
     }
     
   }
